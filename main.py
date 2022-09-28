7c6f59aa128ea13af8fea6501b1d08b31588a335
2426852837 <46857622+2426852837@users.noreply.github.com>
2022-09-28 21:45:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,7 +9,9 @@ import random
 today = datetime.now()
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
+pig_city = os.environ['PIG_CITY']
 birthday = os.environ['BIRTHDAY']
+pig_birthday = os.environ['PIG_BIRTHDAY']
 
 app_id = os.environ["APP_ID"]
 app_secret = os.environ["APP_SECRET"]
@@ -22,7 +24,21 @@ def get_weather():
   url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
   res = requests.get(url).json()
   weather = res['data']['list'][0]
-  return weather['weather'], math.floor(weather['temp'])
+  low=math.floor(weather['low'])
+  high=math.floor(weather['high'])
+  air=weather['airQuality']
+  wind=weather['wind']
+  return weather['weather'], math.floor(weather['temp']),low,high,wind,air
+
+def get_pig_weather():
+  url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + pig_city
+  res = requests.get(url).json()
+  weather = res['data']['list'][0]
+  low = math.floor(weather['low'])
+  high = math.floor(weather['high'])
+  air = weather['airQuality']
+  wind = weather['wind']
+  return weather['weather'], math.floor(weather['temp']),low,high,wind,air
 
 def get_count():
   delta = today - datetime.strptime(start_date, "%Y-%m-%d")
@@ -30,6 +46,12 @@ def get_count():
 
 def get_birthday():
   next = datetime.strptime(str(date.today().year) + "-" + birthday, "%Y-%m-%d")
+  if next < datetime.now():
+    next = next.replace(year=next.year + 1)
+  return (next - today).days
+
+def get_pig_birthday():
+  next = datetime.strptime(str(date.today().year) + "-" + pig_birthday, "%Y-%m-%d")
   if next < datetime.now():
     next = next.replace(year=next.year + 1)
   return (next - today).days
@@ -47,7 +69,8 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature = get_weather()
-data = {"weather":{"value":wea},"temperature":{"value":temperature},"love_days":{"value":get_count()},"birthday_left":{"value":get_birthday()},"words":{"value":get_words(), "color":get_random_color()}}
+wea, temperature,low, high,wind,air= get_weather()
+pig_wea, pig_temperature,pig_low, pig_high,pig_wind,pig_air = get_pig_weather()
+data = {"weather":{"value":wea, "color":get_random_color()},"temperature":{"value":temperature, "color":get_random_color()},"low":{"value":low, "color":get_random_color()},"high":{"value":high, "color":get_random_color()},"wind":{"value":wind, "color":get_random_color()},"air":{"value":air, "color":get_random_color()},"pig_weather":{"value":pig_wea, "color":get_random_color()},"pig_temperature":{"value":pig_temperature, "color":get_random_color()},"pig_low":{"value":pig_low, "color":get_random_color()},"pig_high":{"value":pig_high, "color":get_random_color()},"pig_wind":{"value":pig_wind, "color":get_random_color()},"pig_air":{"value":pig_air, "color":get_random_color()},"love_days":{"value":get_count(), "color":get_random_color()},"birthday_left":{"value":get_birthday(), "color":get_random_color()},"birthday_right":{"value":get_pig_birthday(), "color":get_random_color()},"words":{"value":get_words(), "color":get_random_color()}}
 res = wm.send_template(user_id, template_id, data)
 print(res)
