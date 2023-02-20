bc493ea620a484f315bb88433b959ed485d4b84e
2426852837 <46857622+2426852837@users.noreply.github.com>
2023-02-20 19:15:02 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,9 +7,26 @@ import os
 import random
 
 today = datetime.now()
+# start_date = "2021-12-31"
+# city = "610116"
+# city_name = "西安"
+# pig_city = "110000"
+# pig_city_name = "北京"
+# birthday = "10-23"
+# pig_birthday = "10-15"
+#
+# app_id = "wx4ca6f71d78aab518"
+# app_secret = "1749a84a2c89036d57b13b1de24c107b"
+#
+# user_id = "oX1sI6GC9r8M1aYW9ucfGMUC9JxA"
+# user_id_pig = "oX1sI6OG8ib14dv5Fzj3Dv2Xd1n4"
+# template_id = "dvRvriBJD1hT7YeR6-MQHh74wwN2rgMi5ODGRCV8wZw"
+
 start_date = os.environ['START_DATE']
 city = os.environ['CITY']
+city_name = os.environ['CITY_NAME']
 pig_city = os.environ['PIG_CITY']
+pig_city_name = os.environ['PIG_CITY_NAME']
 birthday = os.environ['BIRTHDAY']
 pig_birthday = os.environ['PIG_BIRTHDAY']
 
@@ -22,25 +39,23 @@ template_id = os.environ['TEMPLATE_ID']
 
 
 def get_weather():
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + city
+    url = "https://restapi.amap.com/v3/weather/weatherInfo?key=d38b5352dc107824ff7a345e210f55cf&city=" + city
     res = requests.get(url).json()
-    weather = res['data']['list'][0]
-    low = math.floor(weather['low'])
-    high = math.floor(weather['high'])
-    air = weather['airQuality']
-    wind = weather['wind']
-    return weather['weather'], math.floor(weather['temp']), low, high, wind, air
+    weather = res['lives'][0]['weather']
+    temperature = res['lives'][0]['temperature']
+    wind = res['lives'][0]['winddirection']
+    wind_power = res['lives'][0]['windpower']
+    return weather, temperature, wind, wind_power
 
 
 def get_pig_weather():
-    url = "http://autodev.openspeech.cn/csp/api/v2.1/weather?openId=aiuicus&clientType=android&sign=android&city=" + pig_city
+    url = "https://restapi.amap.com/v3/weather/weatherInfo?key=d38b5352dc107824ff7a345e210f55cf&city=" + pig_city
     res = requests.get(url).json()
-    weather = res['data']['list'][0]
-    low = math.floor(weather['low'])
-    high = math.floor(weather['high'])
-    air = weather['airQuality']
-    wind = weather['wind']
-    return weather['weather'], math.floor(weather['temp']), low, high, wind, air
+    pig_weather = res['lives'][0]['weather']
+    pig_temperature = res['lives'][0]['temperature']
+    pig_wind = res['lives'][0]['winddirection']
+    pig_wind_power = res['lives'][0]['windpower']
+    return pig_weather, pig_temperature, pig_wind, pig_wind_power
 
 
 def get_count():
@@ -76,19 +91,18 @@ def get_random_color():
 client = WeChatClient(app_id, app_secret)
 
 wm = WeChatMessage(client)
-wea, temperature, low, high, wind, air = get_weather()
-pig_wea, pig_temperature, pig_low, pig_high, pig_wind, pig_air = get_pig_weather()
-data = {"city": {"value": city, "color": get_random_color()}, "weather": {"value": wea, "color": get_random_color()},
+wea, temperature, wind, wind_power = get_weather()
+pig_wea, pig_temperature, pig_wind, pig_wind_power = get_weather()
+data = {"city": {"value": city_name, "color": get_random_color()},
+        "weather": {"value": wea, "color": get_random_color()},
         "temperature": {"value": temperature, "color": get_random_color()},
-        "low": {"value": low, "color": get_random_color()}, "high": {"value": high, "color": get_random_color()},
-        "wind": {"value": wind, "color": get_random_color()}, "air": {"value": air, "color": get_random_color()},
-        "pig_city": {"value": pig_city, "color": get_random_color()},
+        "wind": {"value": wind, "color": get_random_color()},
+        "wind_power": {"value": wind_power, "color": get_random_color()},
+        "pig_city": {"value": pig_city_name, "color": get_random_color()},
         "pig_weather": {"value": pig_wea, "color": get_random_color()},
         "pig_temperature": {"value": pig_temperature, "color": get_random_color()},
-        "pig_low": {"value": pig_low, "color": get_random_color()},
-        "pig_high": {"value": pig_high, "color": get_random_color()},
         "pig_wind": {"value": pig_wind, "color": get_random_color()},
-        "pig_air": {"value": pig_air, "color": get_random_color()},
+        "pig_wind_power": {"value": pig_wind_power, "color": get_random_color()},
         "love_days": {"value": get_count(), "color": get_random_color()},
         "birthday_left": {"value": get_birthday(), "color": get_random_color()},
         "birthday_right": {"value": get_pig_birthday(), "color": get_random_color()},
